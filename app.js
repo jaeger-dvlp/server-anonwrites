2900e323d96c79fe7b4813665abe6d3ddd46b27a
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-11-15 13:51:41 +0300

    Get data from db && send

--- app.js
+++ app.js
@@ -1,124 +1,62 @@
+const MongoClient = require('mongodb').MongoClient
 const express = require('express')
 const app = express()
 const port = 3050
 
-const categories = ['war', 'world', 'politic']
-
-const writes = [
-  {
-    writeId: 0,
-    writeAuthor: 'jaeger-dvlp',
-    writeContent:
-      'Duo prompta dolorem consequat ne, id vis feugait cotidieque? Antiopam partiendo mea ad. Nam agam contentiones ad, odio quodsi sea no, nam ex quod brute disputationi.',
-    writeCategories: ['war'],
-    writeDate: '01/01/2021',
-    writeTime: '00:00'
-  },
-  {
-    writeId: 1,
-    writeAuthor: 'lynicis',
-    writeContent:
-      'Has dicit pericula ea, sed at debitis facilis, nibh placerat eu sea. Aperiam ornatus signiferumque cum in.',
-    writeCategories: ['world', 'war', 'politic'],
-    writeDate: '01/01/2021',
-    writeTime: '00:00'
-  },
-  {
-    writeId: 2,
-    writeAuthor: 'cartApp',
-    writeContent:
-      'Vim id option recteque, epicuri nominavi ne vim. Nam ad vero ferri, has ne homero munere deserunt, munere nusquam et sea.',
-    writeCategories: ['politic'],
-    writeDate: '01/01/2021',
-    writeTime: '00:00'
-  },
-  {
-    writeId: 3,
-    writeAuthor: 'Anonym',
-    writeContent:
-      'Qui summo habemus nominavi et, no simul nostrum vel. Vim alienum delicatissimi id. Ut postea hendrerit scriptorem ius, nam omnis oblique id.',
-    writeCategories: ['world'],
-    writeDate: '01/01/2021',
-    writeTime: '00:00'
-  },
-  {
-    writeId: 4,
-    writeAuthor: 'schiesseGuy',
-    writeContent:
-      'Sonet timeam invenire mei te, cum officiis adipisci mediocritatem ei! Denique referrentur ut nec, ut soleat aeterno menandri duo, euismod nonumes adipiscing ad eos!',
-    writeCategories: ['world'],
-    writeDate: '01/01/2021',
-    writeTime: '00:00'
-  },
-  {
-    writeId: 5,
-    writeAuthor: 'JohnDoe',
-    writeContent:
-      'Has dicit pericula ea, sed at debitis facilis, nibh placerat eu sea. Aperiam ornatus signiferumque cum in.',
-    writeCategories: ['world', 'war', 'politic'],
-    writeDate: '01/01/2021',
-    writeTime: '00:00'
-  },
-  {
-    writeId: 6,
-    writeAuthor: 'Anonym',
-    writeContent:
-      'Vim id option recteque, epicuri nominavi ne vim. Nam ad vero ferri, has ne homero munere deserunt, munere nusquam et sea.',
-    writeCategories: ['world'],
-    writeDate: '01/01/2021',
-    writeTime: '00:00'
-  },
-  {
-    writeId: 7,
-    writeAuthor: 'Anonym',
-    writeContent:
-      'Qui summo habemus nominavi et, no simul nostrum vel. Vim alienum delicatissimi id. Ut postea hendrerit scriptorem ius, nam omnis oblique id.',
-    writeCategories: ['world'],
-    writeDate: '01/01/2021',
-    writeTime: '00:00'
-  },
-  {
-    writeId: 8,
-    writeAuthor: 'Anonym',
-    writeContent:
-      'Sonet timeam invenire mei te, cum officiis adipisci mediocritatem ei! Denique referrentur ut nec, ut soleat aeterno menandri duo, euismod nonumes adipiscing ad eos!',
-    writeCategories: ['world', 'war', 'politic'],
-    writeDate: '01/01/2021',
-    writeTime: '00:00'
-  }
-]
+const uri = 'mongodb://localhost:27017'
 
 let allowCrossDomain = function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET')
   res.setHeader('Access-Control-Allow-Credentials', true)
-
   next()
 }
 app.use(allowCrossDomain)
 
-app.get('/getWrites', (req, res) => {
+const getAllWrites = (res) => {
+  MongoClient.connect(uri, async (err, client) => {
+    let anonDB = client.db('anonwrites').collection('writes')
+    res.json(await anonDB.find().toArray())
+    client.close()
+  })
+}
+
+const getWritesByCategory = (res, category) => {
+  MongoClient.connect(uri, async (err, client) => {
+    let anonDB = client.db('anonwrites').collection('writes')
+    res.json(await anonDB.find({writeCategories: category}).toArray())
+    client.close()
+  })
+}
+
+const getAllWriteCategories = (res) => {
+  MongoClient.connect(uri, async (err, client) => {
+    let anonDB = client.db('anonwrites').collection('categories')
+    res.json(await anonDB.find().toArray())
+    client.close()
+  })
+}
+
+app.get('/getWrites', async (req, res) => {
   setTimeout(() => {
-    res.json(writes)
-  }, 3000)
+    getAllWrites(res)
+  }, 1000)
 })
 
 app.get('/getWrites/category/:categoryName', (req, res) => {
-  let writesToSendByCategory = []
-  writes.map((write) => {
-    if (write.writeCategories.includes(req.params.categoryName) === true) {
-      writesToSendByCategory.push(write)
-    }
-  })
-  setTimeout(() => {
-    res.json(writesToSendByCategory)
-  }, 2000)
+  if (categories.includes(req.params.categoryName) === true) {
+    setTimeout(() => {
+      getWritesByCategory(res, req.params.categoryName)
+    }, 1000)
+  }
 })
 
 app.get('/getCategories', (req, res) => {
-  res.json(categories)
+  setTimeout(() => {
+    getAllWriteCategories(res)
+  }, 500)
 })
-
+3
 app.listen(port, (err) => {
   console.log(`Server is runing at ${port}!`)
 })
