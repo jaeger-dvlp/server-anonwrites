51923d1427ae7ffd6f2ab4b5f2533da5ffd3a1ec
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-11-15 17:28:46 +0300

    Add cors & handle post reqs & push data to db

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 const MongoClient = require('mongodb').MongoClient
+const cors = require('cors')
 const express = require('express')
 const app = express()
 const port = 3050
 
 const uri = 'mongodb://localhost:27017'
 
+const categories = [
+  'world',
+  'war',
+  'politic',
+  'religion',
+  'usa',
+  'science',
+  'tech'
+]
+
 let allowCrossDomain = function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST')
   res.setHeader('Access-Control-Allow-Credentials', true)
   next()
 }
+app.use(cors())
 app.use(allowCrossDomain)
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 
 const getAllWrites = (res) => {
   MongoClient.connect(uri, async (err, client) => {
@@ -37,6 +51,18 @@ const getAllWriteCategories = (res) => {
   })
 }
 
+const pushNewWrite = (data, res) => {
+  MongoClient.connect(uri, async (err, client) => {
+    let anonDB = client.db('anonwrites').collection('writes')
+    await anonDB.insertOne({
+      writeContent: data.write,
+      writeCategories: data.categories
+    })
+    await res.json('Successful!')
+    await client.close()
+  })
+}
+
 app.get('/getWrites', async (req, res) => {
   setTimeout(() => {
     getAllWrites(res)
@@ -56,7 +82,15 @@ app.get('/getCategories', (req, res) => {
     getAllWriteCategories(res)
   })
 })
-3
+
+app.post('/newWrite', (req, res) => {
+  req.body !== undefined
+    ? setTimeout(() => {
+        pushNewWrite(req.body, res)
+      }, 2000)
+    : ''
+})
+
 app.listen(port, (err) => {
   console.log(`Server is runing at ${port}!`)
 })
